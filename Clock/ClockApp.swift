2aa6ee660cbcb7cb9a04426ac1de961374d5bc2f
alexandr_sadunenko <a.sadunenko@gmail.com>
2021-11-27 21:08:52 +0200

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Alex on 27.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
